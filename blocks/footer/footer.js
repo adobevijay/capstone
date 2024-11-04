d34a90fcf82bda8ca5c20c8c254fc4d29707736a
Vijay Gehlot <433195@conneqtcorp.com>
2024-11-04 22:30:38 +0530

    footer update

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -3,19 +3,17 @@ import { loadFragment } from '../fragment/fragment.js';
 
 /**
  * loads and decorates the footer
- * @param {Element} block The footer block element
+ * @param {Element} block 
  */
 export default async function decorate(block) {
+  
   const footerMeta = getMetadata('footer');
-  block.textContent = '';
-
-  // load footer fragment
-  const footerPath = footerMeta.footer || '/footer';
+  const footerPath = footerMeta ? new URL(footerMeta, window.location).pathname : '/footer';
   const fragment = await loadFragment(footerPath);
 
-  // decorate footer DOM
+ 
+  block.textContent = '';
   const footer = document.createElement('div');
-  footer.classList.add('wrapper');
   while (fragment.firstElementChild) footer.append(fragment.firstElementChild);
 
   block.append(footer);
